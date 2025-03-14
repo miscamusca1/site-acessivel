fd2f7ed8fc9682498410998f9d6d6a9c7da2d5ff
miscamusca1 <159577313+miscamusca1@users.noreply.github.com>
2025-03-14 19:08:41 +0000

    rotação

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
     const botaoDeacessibilidade = document.getElementById('botaoDeacessibilidade');
-    const opcoesDeacessibilidade = document.getElementById(opcoesDeacessibilidade');
-
+    const opcoesDeacessibilidade = document.getElementById('opcoesDeacessibilidade');
+botaoDeacessibilidade.addEventListener('click', function(){
+    botaoDeacessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeacessibilidade.classList.toggle('apresenta-lista');
+});
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
