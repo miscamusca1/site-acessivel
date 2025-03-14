a2b6ad5d15ff523a48bb77c3567438ad496fe4c8
miscamusca1 <159577313+miscamusca1@users.noreply.github.com>
2025-03-14 17:11:45 +0000

    .

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
